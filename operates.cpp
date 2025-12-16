5590b2260df2e18645f0a0db517c4fc72aa4c802
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2025-12-16 21:54:01 +0600

    Add files via upload

---
+++ operates.cpp
@@ -0,0 +1,25 @@
+/* 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int savings = 100;
+    savings += 50 ;
+    cout << "Total savings : " <<savings;
+    return 0 ;
+    }
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    x +=5;
+    cout << x << "\n";
+    x *=2;
+    cout << x << "\n";
+    return 0;
+    
+}
