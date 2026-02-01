7b9e796ded48dc518411aa38f793400c730b184b
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-01 22:23:12 +0600

    Add files via upload

--- omit array.cpp
+++ omit array.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 using namespace std;
@@ -14,4 +15,37 @@ int main(){
     }
     return 0;
 
+}
+    
+
+//Arrays - Fixed Size Example
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string cars[3]={"volvo","BMW","Ford"}
+
+    cars[3]="Mazda";
+    for (string car:cars){
+        cout <<car << \"n";
+    }
+    return 0;
+
+}
+    */
+
+//add a vector array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <string> cars = {"volvo","BMW","Ford"};
+    cars.push_back("Tesla");
+    for (string car:cars){
+        cout <<car <<"\n";
+    }
+    return 0;
 }
