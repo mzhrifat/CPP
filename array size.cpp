bd840dcc04070e32d732a55e44e32555ac57fd74
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-01 22:48:46 +0600

    Add files via upload

--- array size.cpp
+++ array size.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -6,5 +7,17 @@ int main(){
     int getArrayLength = sizeof(myNumbers)/sizeof(myNumbers[0]);
     cout << getArrayLength;
     return 0;
-    
+
+}
+    */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int myNumbers[5]={10,20,30,40,50};
+    for (int i =0;i<sizeof(myNumbers)/sizeof(myNumbers[0]);i++){
+        cout << myNumbers[i]<< "\n";
+
+    }
 }
