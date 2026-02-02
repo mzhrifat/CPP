f438dcd68692983a8dccc6b43b304dcf58cc7174
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-02 22:56:23 +0600

    Add files via upload

--- Multi-Dimensional Arrays.cpp
+++ Multi-Dimensional Arrays.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -8,5 +9,40 @@ int main(){
   };
   
   cout << letters[0][2];
+  return 0;
+}
+
+
+//Change Elements in a Multi-Dimensional Array
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string letters[2][4] = {
+    { "A", "B", "C", "D" },
+    { "E", "F", "G", "H" }
+  };
+  letters[0][0] = "z";
+
+  cout << letters [0] [0];
+  return 0;
+}
+*/
+
+//Loop Through a Multi-Dimensional Array
+#include <iostream>
+using namespace std;
+
+int main(){
+    string letters[2][4] = {
+    { "A", "B", "C", "D" },
+    { "E", "F", "G", "H" }
+  };
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 4; j++) {
+      cout << letters[i][j] << "\n";
+    }
+  }
   return 0;
 }
