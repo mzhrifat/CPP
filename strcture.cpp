b70c5c43790f8e1b707bd1f7216befca39aebf62
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-05 00:00:34 +0600

    Add files via upload
    Structure commit

--- strcture.cpp
+++ strcture.cpp
@@ -45,8 +45,8 @@ int main(){
 
     return 0;
 }
- */
  
+
 // One Structure in Multiple Variables
 
 #include <iostream>
@@ -73,5 +73,39 @@ int main() {
   // Print the structure members
   cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
   cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+  return 0;
+}
+*/
+
+//Use one structure to represent two cars
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Declare a structure named "car"
+struct car {
+  string brand;
+  string model;
+  int year;
+};
+
+int main() {
+  // Create a car structure and store it in myCar1;
+  car myCar1;
+  myCar1.brand = "BMW";
+  myCar1.model = "X5";
+  myCar1.year = 1999;
+
+  // Create another car structure and store it in myCar2;
+  car myCar2;
+  myCar2.brand = "Ford";
+  myCar2.model = "Mustang";
+  myCar2.year = 1969;
+ 
+  // Print the structure members
+  cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+  cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+ 
   return 0;
 }
