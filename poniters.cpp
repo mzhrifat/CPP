aed38c2676cb4736449113d7f0864501eba712e6
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-18 00:12:04 +0600

    pointers

--- poniters.cpp
+++ poniters.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,4 +9,21 @@ int main(){
     cout << food << "\n";
     cout <<&food << "\n";
     return 0 ;
+}
+    */
+//2 pointer
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string food = "pizza";
+    string * ptr = &food ;
+    cout << food << "\n";
+    cout << &food << "\n";
+
+    cout << ptr << "\n";
+
+    return 0;
 }
