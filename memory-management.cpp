a2b844d3b970dc2bb36ef88ab789f8458aff6fff
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-17 23:41:18 +0600

    Add files via upload

---
+++ memory-management.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myInt;
+  float myFloat;
+  double myDouble;
+  char myChar;
+
+  cout << sizeof(myInt) << "\n";     // 4 bytes (typically)
+  cout << sizeof(myFloat) << "\n";   // 4 bytes
+  cout << sizeof(myDouble) << "\n";  // 8 bytes
+  cout << sizeof(myChar) << "\n";    // 1 byte
+  return 0;
+}
