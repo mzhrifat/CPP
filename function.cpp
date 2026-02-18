896b95f765669cfc705806753be2c5e99306ba8b
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-18 23:46:33 +0600

    function 2 commit

--- function.cpp
+++ function.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -8,4 +9,19 @@ void myFunction(){
 int main(){
     myFunction();
     return 0 ;
+}
+    */
+//2
+#include <iostream>
+using namespace std;
+
+void myFunction(){
+    cout << "HEllo\n";
+
+}
+int main(){
+    myFunction();
+    myFunction();
+    myFunction();
+    return 0;
 }
