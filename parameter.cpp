4f0fb8ec26be967a65a1759a398fdf4fd371f84c
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-19 00:32:55 +0600

    Default parameter

--- parameter.cpp
+++ parameter.cpp
@@ -1,14 +1,32 @@
+/*
 #include <iostream>
 #include <string>
 using namespace std;
 
-void myFunction(string fname) {
-  cout << fname << " Refsnes\n";
+void myFunction(string fname){
+    cout << fname << "Refsnes\n";
 }
 
-int main() {
-  myFunction("Liam");
-  myFunction("Jenny");
-  myFunction("Anja");
-  return 0;
+int main(){
+    myFunction("Lima");
+    myFunction ("Zarin");
+    myFunction("Rafa");
+    return 0;
+}
+    */
+
+//Default parameter
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string country = "Norway"){
+    cout << country << "\n";
+}
+int main(){
+    myFunction("India");
+    myFunction("BD");
+    myFunction("Austia");
+    return 0;
 }
