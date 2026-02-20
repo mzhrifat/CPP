fa468283f5b934cec3c2ab0b84fae6f448740af3
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-20 23:41:49 +0600

    Add files via upload
    2 integer number

--- pass by reference.cpp
+++ pass by reference.cpp
@@ -1,4 +1,4 @@
-
+/*
 //Pass an integer by reference:
 #include <iostream>
 #include <string>
@@ -12,4 +12,29 @@ int main(){
     changeValue(value);
     cout << value;
     return 0;
+}
+*/
+
+//Pass two integers by reference:
+
+#include <iostream>
+using namespace std;
+
+void swapNums(int &x , int &y){
+    int z= x;
+    x=y;
+    y=z;
+}
+
+int main(){
+    int firstNum = 10;
+    int secondNum = 20;
+    cout << "Before swap:"<< "\n";
+    cout << firstNum << secondNum << "\n";
+    swapNums(firstNum ,secondNum);
+    cout << "After swap:" << "\n";
+    cout << firstNum << secondNum << "\n";
+
+    return 0;
+
 }
