9ac4c7f85b68fedf276a2d545564b73b4ca4188e
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-22 00:26:44 +0600

    Add files via upload
    Function Overloading by Number of Parameters

--- without function overloading.cpp
+++ without function overloading.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -14,5 +15,26 @@ int main(){
     double myNum2 = plusFuncDouble(4.3,6.26);
     cout << "Int:"<< myNum1 << "\n";
     cout <<"Double:"<<myNum2;
+    return 0;
+}
+*/
+
+//Function Overloading by Number of Parameters
+#include <iostream>
+using namespace std;
+
+int plusFunc(int x, int y){
+    return x+y;
+}
+int plusFunc(int x,int y,int z){
+    return x+y+z;
+}
+int main(){
+    int result1= plusFunc(3,7);
+    int result2=plusFunc(1,2,4);
+
+    cout << "Sum of 2 numbers:"<<result1 <<"\n";
+    cout << "Sum of 3 numbers:"<<result2 << "\n";
+
     return 0;
 }
