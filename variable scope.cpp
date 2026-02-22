98b8bce7847cdd6d318ecd67204473e2a4717bbd
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-22 23:48:34 +0600

    a function is global and can therefore be used by any

--- variable scope.cpp
+++ variable scope.cpp
@@ -1,5 +1,6 @@
-
+/*
 //A variable created inside a function belongs to the local scope of that function, and can only be used inside that function
+
 #include <iostream>
 using namespace std;
 
@@ -11,5 +12,44 @@ void myFunction(){
 }
 int main(){
     myFunction();
+    return 0;
+}
+
+
+//2
+//A local variable cannot be used outside the function it belongs to.
+// If you try to access it outside the function, an error occurs:
+
+#include <iostream>
+using namespace std;
+
+void myFunction(){
+    int x=5;
+    cout << x;
+}
+int main(){
+    myFunction();
+    cout << x;
+    return 0;
+}
+    */
+
+
+    //3
+
+//A variable created outside of a function is global and can therefore be used by anyone:
+#include <iostream>
+using namespace std;
+//Global variable x
+int x= 5;
+
+void myFunction(){
+    //we can use x here
+    cout << x<<"\n";
+
+}
+int main(){
+    myFunction();
+    cout << x;
     return 0;
 }
