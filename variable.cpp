7393c22b8124d884dfdc178dba3f9c01e7979046
MD.ZIAUL HASHAN RIFAT <153647130+mzhrifat@users.noreply.github.com>
2026-02-22 23:42:08 +0600

    Add files via upload
    A local variable cannot be used outside the function it belongs to.  If you try to access it outside the function, an error occurs:

---
+++ variable.cpp
@@ -0,0 +1,25 @@
+/*
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,z;
+    x=y=z=50;
+    cout << x+y+z;
+    return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int minutesPerHour = 60;
+    int m=60 ;
+
+cout << minutesPerHour << "\n";
+
+cout << m;
+return 0;
+}
